876caa70f8e2a89980aa7a32cdf86f2db701c14d
berrygobbler78 <127357762+berrygobbler78@users.noreply.github.com>
2023-03-21 20:58:37 -0700

    Graphing works

--- Math Solver Thing/Commands/Graphing/graphPanel.java
+++ Math Solver Thing/Commands/Graphing/graphPanel.java
@@ -16,8 +16,8 @@ public class graphPanel extends JPanel {
         this.setPreferredSize(new Dimension(h, w));
     }
 
-    public void paint(Graphics g) {
-        int scale = 10;
+    public void paintComponent(Graphics g) {
+        double scale = 10;
 
         // Creates the x and y axis
         Graphics2D g1 = (Graphics2D) g;
@@ -30,6 +30,8 @@ public class graphPanel extends JPanel {
         // double a = qG.a;
         // double b = qG.b;
         // double c = qG.c;
+
+        // 5x^2+-7x+-2
         double a = 5;
         double b = -7;
         double c = -2;
@@ -40,9 +42,9 @@ public class graphPanel extends JPanel {
         g2.setColor(Color.red);
 
         Polygon p1 = new Polygon();
-        for (int x = -250; x <= 250; x++) {
-            p1.addPoint(originX + x*scale, originY - scale * ((int) Math.round(a*(x*x)+b*x+c)));
-          }
+        for (double x = -w; x <= w; x += 0.1) {
+            p1.addPoint((int) Math.round(originX + x*scale), (int)Math.round(originY - scale * (a*(x*x)+b*x+c)));
+        }
           
         g2.drawPolyline(p1.xpoints, p1.ypoints, p1.npoints);
     }
